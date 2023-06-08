e1745be11d1444d9914b8460cebbec2dbfd9a37b
Ayoub Chegraoui <ayoubch807@gmail.com>
2023-06-08 18:23:27 +0100

    add toggles for the parts to hide in youtube home page

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
+const wholePageSelector = 'ytd-rich-grid-renderer';
+const shortsSelector = '.ytd-rich-section-renderer';
+const rowLimit = 1;
+
 const hideElement = (identifier) => {
-    let element = document.querySelector(identifier);
-    if(element && element.style) element.style.display = 'none';
+	let element = document.querySelector(identifier);
+	if (element && element.style) element.style.display = 'none';
 }
 
-
-setInterval(() => {
-    hideElement('ytd-rich-grid-renderer');
-    hideElement('#secondary');
-}, 1000);
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	console.log(request, sender, sendResponse);
+	const { element, state } = request;
+	alert("Got a message from PopUp");
+	if (element === 'all') {
+		hideElement(wholePageSelector);
+	}
+	else if (element === 'shorts') {
+		hideElement(shortsSelector);
+	}
+	// setInterval(() => {
+	//     hideElement(wholePageSelector);
+	//     // hideElement('#secondary');
+	//     // hideElement(shortsSelector);
+	// }, 1000);
+});
